5ffd577d85f27cccfd5c6c227c197a99e32fd0d1
Ubuntu <ubuntu@ip-172-31-91-42.ec2.internal>
2021-11-25 17:41:07 +0000

    Update Hello  world with Style

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Logan Sweeney
+# Date: November 25, 2021
+# This program displays Hello, World! with proper style.
+
+def main():
+    print("Hello, World! with proper style!")
+
+
+if __name__ == "__main__":
+    main()
